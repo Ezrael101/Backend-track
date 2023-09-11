9f27133f9483439cdd0c8dfee35b4a2095f7e714
Ezrael101 <144630530+Ezrael101@users.noreply.github.com>
2023-09-11 09:06:41 +0100

    Add files via upload

---
+++ endpoint.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_GET['slack_name']) && isset($_GET['track'])) {
+    // Retrieve the values in the GET request
+    $slack_name = $_GET['slack_name'];
+    $track = $_GET['track'];
+	// Assign status code
+	$statusCode = 200;
+	$currentDayOfWeek = gmdate('l');
+	$utcTime = gmdate("Y-m-d\TH:i:s\Z");
+
+	$response = array(
+    'slack_name' => $slackName,
+    'current_day' => $currentDayOfWeek,
+    'utc_time' => $utcTime,
+	'track' => $track,
+    'github_file_url' => $githubFileUrl,
+    'github_source_code_url' => $githubSourceCodeUrl,
+    'status_code' => $statusCode
+);
+    header('Content-Type: application/json');
+
+    echo json_encode($response);
+} else {
+    // error message
+    $errorResponse = array('error' => 'Slack name and track are required.');
+    header('Content-Type: application/json');
+    echo json_encode($errorResponse);
+}
+?>
